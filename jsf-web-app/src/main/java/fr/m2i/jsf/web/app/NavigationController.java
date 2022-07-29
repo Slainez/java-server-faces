d6266c9f11e8a07289000324e61f3dbeb73f22b4
Slainez <sullyvan360@gmail.com>
2022-07-29 15:10:08 +0200

    ajout de redirection conditionelle

--- jsf-web-app/src/main/java/fr/m2i/jsf/web/app/NavigationController.java
+++ jsf-web-app/src/main/java/fr/m2i/jsf/web/app/NavigationController.java
@@ -2,10 +2,33 @@
 package fr.m2i.jsf.web.app;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 @ManagedBean(name="navigationController" , eager = true)
 public class NavigationController {
     
+    @ManagedProperty(value="#{param.pageId}")
+    private String pageId ;
+    
+    
+    
+    public String goToPage(){
+        if(pageId == null){
+            return "home";
+        }
+        
+        return pageId ;
+    }
+
+    public String getPageId() {
+        return pageId;
+    }
+
+    public void setPageId(String pageId) {
+        this.pageId = pageId;
+    }
+    
+        
     public String moveToHelloPage(){
         
         return "hello";
